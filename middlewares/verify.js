aec39d4ea92f5745213871b2f96910c1b8032f6e
unknown <particular6@proton.me>
2024-03-18 17:52:39 +0500

    Changed the verification middleware to a Passport.js strategy instead of a custom one.

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -1,24 +1,17 @@
-const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
 const secretKey = process.env.SECRET;
 
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secretKey
+};
 
-const verify = function (req, res, next) {
-    let token = req.headers.Authorization || req.headers.authorization;
-    if (!token) {
-        console.error('wrong token');
-        return res.status(400).json({ message: "wrong token!!" });
-    }
+passport.use(new Strategy(options, (payload, done) => {
+  
+  return done(null, payload);
+}));
 
-    token = token.split(' ')[1];
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        console.log(decoded);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).json({ message: "wrong token!!" });
-    }
-
-    next();
-}
+const verify = passport.authenticate('jwt', { session: false });
 
 module.exports = verify;
